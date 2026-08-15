2e6f4723614147500dcececb309c567a689add72
parmlabs01 <parmlabsltd@gmail.com>
2026-08-15 23:02:08 +0100

    Update build.sh to include .env setup
    Added environment variable configuration to build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 _flutter_sdk
-./_flutter_sdk/bin/flutter config --enable-web
-./_flutter_sdk/bin/flutter create . --platforms=web
-./_flutter_sdk/bin/flutter pub get
-./_flutter_sdk/bin/flutter build web --release
+
+cat > .env << EOF
+SUPABASE_URL=$SUPABASE_URL
+SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+OPENAI_API_KEY=$OPENAI_API_KEY
+GEMINI_API_KEY=$GEMINI_API_KEY
+STABILITY_API_KEY=$STABILITY_API_KEY
+FLUX_API_KEY=$FLUX_API_KEY
+EOF
+
+flutter create . --platforms=web
+flutter pub get
+flutter build web --release
