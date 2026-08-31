11ff6755f95024ad3f97c68c6180dfe60f1cd2c5
parmlabs01 <parmlabsltd@gmail.com>
2026-08-31 23:09:48 +0100

    Add multiple state providers for settings

--- lib/shared_settings_provider.dart
+++ lib/shared_settings_provider.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final languageProvider = StateProvider<String>((ref) => 'English');
-final notificationsEnabledProvider = StateProvider<bool>((ref) => true);
+final personalizationStyleProvider = StateProvider<String>((ref) => 'Balanced');
+final contentFiltersEnabledProvider = StateProvider<bool>((ref) => true);
+final accentColorProvider = StateProvider<Color>((ref) => const Color(0xFF3B82F6));
+final emailNotificationsProvider = StateProvider<bool>((ref) => true);
+final pushNotificationsProvider = StateProvider<bool>((ref) => true);
+final defaultExportFormatProvider = StateProvider<String>((ref) => 'PNG');
+final hdExportEnabledProvider = StateProvider<bool>((ref) => false);
+final dataControlsEnabledProvider = StateProvider<bool>((ref) => true);
